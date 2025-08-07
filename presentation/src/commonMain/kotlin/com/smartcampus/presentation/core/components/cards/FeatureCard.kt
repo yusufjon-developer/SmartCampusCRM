e1006ac043fe1393e7b2f1b264b9d64d28aeb860
Yusufjon <it.frister@gmail.com>
2025-08-07 06:43:47 +0500

    feat(ui): add FeatureCard

--- presentation/src/commonMain/kotlin/com/smartcampus/presentation/core/components/cards/FeatureCard.kt
+++ presentation/src/commonMain/kotlin/com/smartcampus/presentation/core/components/cards/FeatureCard.kt
@@ -3,36 +3,30 @@ package com.smartcampus.presentation.core.components.cards
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import coil3.compose.rememberAsyncImagePainter
 
 @Composable
-fun UserCard(
+fun FeatureCard(
     modifier: Modifier = Modifier,
-    imageUrl: String,
+    icon: Painter,
     title: String,
-    role: String,
-    isActive: Boolean,
     onClick: () -> Unit
 ) {
     Card(
@@ -43,43 +37,30 @@ fun UserCard(
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = rememberAsyncImagePainter(imageUrl),
+                painter = icon,
                 contentDescription = "Avatar",
                 modifier = Modifier
-                    .size(144.dp)
+                    .padding(48.dp)
+                    .size(96.dp)
                     .clip(RoundedCornerShape(12.dp)),
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.FillWidth
             )
 
 
             Spacer(modifier = Modifier.width(16.dp))
 
-            Row {
-                Box(
-                    modifier = Modifier
-                        .size(20.dp)
-                        .padding(4.dp)
-                        .clip(CircleShape)
-                        .background(if (isActive) Color.Green else MaterialTheme.colorScheme.error)
-                        .offset(x = 16.dp, y = 16.dp)
-                )
-
-                Column {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = role,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-                }
-            }
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
         }
     }
 }
